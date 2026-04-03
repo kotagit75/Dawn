5750ae91237244af1ee95d1ce51949dd54f237e6
kotagit75 <unity75lets5@gmail.com>
2026-04-03 15:07:14 +0900

    add functions to signature: verify, sign

--- src/util/signature.rs
+++ src/util/signature.rs
@@ -1,1 +1,27 @@
+use openssl::{
+    error::ErrorStack,
+    hash::MessageDigest,
+    sign::{Signer, Verifier},
+};
+
+use crate::util::key::{PK, SK};
+
 pub type Signature = Vec<u8>;
+
+pub fn sign(data: &[u8], sk: SK) -> Result<Signature, ErrorStack> {
+    Signer::new(MessageDigest::sha256(), &sk.key()).and_then(|mut signer| {
+        match signer.update(data) {
+            Ok(_) => signer.sign_to_vec(),
+            Err(e) => Err(e),
+        }
+    })
+}
+
+pub fn verify(data: &[u8], pk: PK, signature: Signature) -> bool {
+    Verifier::new(MessageDigest::sha256(), &pk.key())
+        .and_then(|mut verifyer| match verifyer.update(data) {
+            Ok(_) => verifyer.verify(&signature),
+            Err(e) => Err(e),
+        })
+        .is_ok()
+}
