5d9d0d6c2f3b6d0b4fd6716b9c31268dcd920af7
kotagit75 <unity75lets5@gmail.com>
2026-04-04 10:19:20 +0900

    added function: get_beacon

--- src/beacon.rs
+++ src/beacon.rs
@@ -1,6 +1,22 @@
+use std::{env::current_dir, process::Command};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct Beacon {
-    pub value: u32,
+    pub value: i32,
+}
+
+const BEACON_COMMAND: &str = "./beacon.sh";
+
+pub fn get_beacon() -> Option<Beacon> {
+    match current_dir() {
+        Ok(x) => Command::new(BEACON_COMMAND)
+            .current_dir(x)
+            .status()
+            .ok()
+            .and_then(|status| status.code()),
+        Err(_) => None,
+    }
+    .map(|value| Beacon { value })
 }
