878ca75034442a0cf6b76e0dc171216b30348c12
kotagit75 <unity75lets5@gmail.com>
2026-08-14 16:08:04 +0900

    feat: implemented Default trait for Config

--- src/config.rs
+++ src/config.rs
@@ -8,3 +8,17 @@ pub struct Config {
     pub beacon_cmd: Vec<String>,
     pub vdf_difficulty: u64,
 }
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            vdf_difficulty: 10,
+            beacon_cmd: Vec::new(),
+            mining: false,
+            peer: Vec::new(),
+            api_port: 8080,
+            p2p_port: 8081,
+            beacon_timeout: 10,
+        }
+    }
+}
